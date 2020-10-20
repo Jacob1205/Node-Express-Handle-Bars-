741e1ef06654005490f941a84f0ca74b8fb4b4a0
Jacob Torres <simplyjacob00@gmail.com>
2020-10-20 18:03:13 -0400

    added burger controller

---
+++ controller/burgerController.js
@@ -0,0 +1,42 @@
+var burger = require("../models/burger.js");
+var express = require("express");
+var router = express.Router();
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var burgerObject = {
+      burgers: data,
+    };
+    res.render("index", burgerObject);
+  });
+});
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(["burger_name"], [req.body.burger_name], function (result) {
+    res.json({ id: result.insertId });
+  });
+});
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+router.delete("/api/burgers/all", function (req, res) {
+  burger.deleteAll(function (result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+module.exports = router;
