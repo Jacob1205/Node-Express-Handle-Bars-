93778ef7f24bc23c109fa9840ceb5647db80e590
Jacob Torres <simplyjacob00@gmail.com>
2020-10-20 18:02:32 -0400

    js files updated

---
+++ config/orm.js
@@ -0,0 +1,71 @@
+var connection = require("./connection.js");
+function generateQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?").toString();
+  }
+  return arr;
+}
+function objectToSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value).toString();
+    }
+  }
+  return arr;
+}
+var orm = {
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += generateQuestionMarks(vals.length);
+    queryString += ") ";
+    connection.query(queryString, vals, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objectToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  delete: function (table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  deleteAll: function (table, cb) {
+    var queryString = "DELETE FROM " + table;
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+};
+module.exports = orm;
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burger_controller");
+var PORT = process.env.PORT || 8080;
+var app = express();
+app
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(express.static(__dirname + "/public"))
+  .use(routes);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
